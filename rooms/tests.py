2637c1a1298d0800ef30beba81eefaf6be16f740
goeasyonng <meoa1026@gmail.com>
2023-04-03 22:30:25 +0900

    04-03

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,6 +1,46 @@
 from rest_framework.test import APITestCase
+from . import models
 
 
 class TestAmenities(APITestCase):
-    def test_two(self):
-        self.assertEqual(2 + 2, 4, "잘못된 계산")
+
+    NAME = "Amenity Test"
+    DESC = "Amenity Des"
+    URL = "/api/v1/rooms/amenities/"
+
+    def setUp(self):
+        models.Amenity.objects.create(
+            name=self.NAME,
+            description=self.DESC,
+        )
+
+    def test_all_amenities(self):
+
+        response = self.client.get(self.URL)
+        data = response.json()
+
+        self.assertEqual(
+            response.status_code,
+            200,
+            "status isn't 200",
+        )
+        self.assertIsInstance(data, list)
+        self.assertEqual(len(data), 1)
+        self.assertEqual(
+            data[0]["name"],
+            self.NAME,
+        )
+        self.assertEqual(data[0]["description"], self.DESC)
+
+    def test_create_amenity(self):
+        response = self.client.post(
+            self.URL,
+            data={"name": "New Amenity", "description": "New Amenity desc"},
+        )
+        data = response.json()
+
+        self.assertEqual(
+            response.status_code,
+            200,
+            "Not 200 status code",
+        )
