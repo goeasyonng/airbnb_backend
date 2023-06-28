3e8894740dad3db17f24ef8d5d8b9d7bd0753d91
goeasyonng <121272470+goeasyonng@users.noreply.github.com>
2023-06-28 23:29:20 +0900

    Update tests.py

--- users/tests.py
+++ users/tests.py
@@ -1,8 +1,1 @@
 from django.test import TestCase
-
-# Create your tests here.
-# test code
-from restframework import test
-from django import restcode
-from django import testcode
-from django.test import TestCase
