67b2aff5a3514414fd72b36228b2dde85b9fd953
goeasyonng <121272470+goeasyonng@users.noreply.github.com>
2023-07-11 23:05:50 +0900

    Update tests.py

--- users/tests.py
+++ users/tests.py
@@ -1,1 +1,14 @@
 from django.test import TestCase
+from .models import User
+
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(name="lion")
+        User.objects.create(name="cat")
+
+    def test_user_get(self):
+        lion = User.objects.get(name="lion")
+        cat = User.objects.get(name="cat")
+        self.assertEqual(lion)
+        self.assertEqual(cat)
